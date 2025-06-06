c5f498e0dff69bc73c4c79bf298f539038274a8b
Jorge Augusto Dev <jorgeguto87@gmail.com>
2025-06-06 00:53:13 -0300

    atualizacao_subpagina_um

--- app.js
+++ app.js
@@ -21,3 +21,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 5000); 
   });
 });
+
+function funcaoMenu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
